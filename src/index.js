c2c734cca697fd5b6c1da5bf12a0e4f3b4084239
Anastasiya <kaiadelina@gmail.com>
2023-10-28 17:30:12 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const matchingBrackets = new Map();
+  const sameBrackets = new Set();
+
+  for (const [open, close] of bracketsConfig) {
+    matchingBrackets.set(close, open);
+    if (open === close) {
+      sameBrackets.add(open);
+    }
+  }
+
+  for (const bracket of str) {
+    if (sameBrackets.has(bracket)) {
+      if (stack.length === 0 || stack[stack.length - 1] !== bracket) {
+        stack.push(bracket);
+      } else {
+        stack.pop();
+      }
+    } else if (matchingBrackets.has(bracket)) {
+      const openBracket = matchingBrackets.get(bracket);
+      if (stack.length === 0 || stack.pop() !== openBracket) {
+        return false;
+      }
+    } else {
+      stack.push(bracket);
+    }
+  }
+
+  return stack.length === 0;
+};
